630c092eb00ebeca1fcd71bd53440872d3c90074
M. Abdullah Abaid <114490415+mabdullah-ac@users.noreply.github.com>
2023-04-13 14:53:18 +0500

    Scroll-bar style and nav label fix.
    Scroll-bar style and nav label fix.

--- src/components/LeftNavTop/LeftNavTop.jsx
+++ src/components/LeftNavTop/LeftNavTop.jsx
@@ -1,7 +1,8 @@
-import "./LeftNavTop.scss"
+import "./LeftNavTop.scss";
 import { NavLink } from "react-router-dom";
 import HomeIcon from "../../assets/home-alt.svg";
 import NftIcon from "../../assets/nft-icon.svg";
+import UserIcon from "../../assets/user.svg";
 import Swapup from "../../assets/swapup.png";
 
 function LeftNavTop() {
@@ -12,13 +13,17 @@ function LeftNavTop() {
       </div>
 
       <nav className="nav">
-        <NavLink to="">
+        {/* <NavLink to="">
           <img src={HomeIcon} alt="Home Icon" />
           <span>Home</span>
-        </NavLink>
+        </NavLink> */}
         <NavLink to="swap">
-          <img src={NftIcon} alt="NFT Icon" />
+          <img src={NftIcon} alt="NFT Icon" className="nft-icon" />
           <span>Swap NFTs</span>
+        </NavLink>
+        <NavLink to="">
+          <img src={UserIcon} alt="User Icon" className="user-icon" />
+          <span>Account</span>
         </NavLink>
       </nav>
     </div>
